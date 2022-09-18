95109d3d0b89a72ce7166fe5c1d7e56fa6aadfc8
lizi <1360080816@qq.com>
2022-09-18 16:38:03 +0800

    update

--- study/day01.py
+++ study/day01.py
@@ -1,19 +1,25 @@
- # go语言 http://www.go.com/study abcd
-
+# coding=utf-8
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+# go语言 http://www.go.com/study abcd
 
 import json
 
-
 if __name__ == '__main__':
-    file =open('/Users/lizi/Desktop/pythonProject/resource/极客时间.txt')
-    file1 =open('/Users/lizi/Desktop/pythonProject/resource/res.txt', mode='a')
+    file =open('/Users/lizi/project/pythonDemo/resource/极客时间.txt')
+    file1 =open('/Users/lizi/project/pythonDemo/resource/res.txt', mode='a')
     data =file.read()
     # data = file.readline()
     list = data.split('\n')
     for i in list:
-        dict = json.loads(i)
-        line = dict['show_name'] +' '+ dict['share_url']+' ' + dict['pwd'] + "\n"
-        file1.write(line)
+        try:
+            dict = json.loads(i)
+            line = dict['show_name'] +' '+ dict['share_url']+' ' + dict['pwd'] + "\n"
+            file1.write(line)
+        except Exception as e:
+            print(e)
+
 
 
 
