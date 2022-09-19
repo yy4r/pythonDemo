c20824aa24d6e73810ef180124d17fba52a184e1
jack <1360080816@qq.com>
2022-09-19 23:31:51 +0800

    update

--- study/day02.py
+++ study/day02.py
@@ -2,3 +2,15 @@
 
 
 # 请求100次百度、将百度的返回结果写进res.txt
+
+
+
+# 统计city中各个省份所含的城市数量、 按照从多到少排列。并且增加一列count （省份、城市、数量）输出为excel/jsonarray
+# 天津 天津 1
+
+
+
+# 将city合并province_num、增加一列province_num省份号 （省份、城市、城市号）输出为excel/json
+
+
+
